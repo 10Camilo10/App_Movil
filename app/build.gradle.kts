40344a64f70366ff0b91ace841571268d10aad37
Camilo Mora <camilo.mora@excellentiam.co>
2025-03-05 20:04:41 -0500

    Activties Registro, Perfil y Recuperar Contraseña
    Se corrigieron algunos detalles, inclusion de intents y logs, y el desarrollo de las nuevas 3 pantallas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.Spikeysanju:MotionToast:1.4")
+    implementation("com.google.android.material:material:1.12.0")
+
 }
